764c35c1697a31cb02a7304c8108bbf27cf7ce32
Alan Shaw <alan.shaw@protocol.ai>
2022-07-05 23:53:26 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Miniswap
+
+Miniature Bitswap implementation, remote read only.
+
+## Usage
+
+```js
+import { Miniswap, BITSWAP_PROTOCOL } from 'miniswap'
+
+const miniswap = new Miniswap(libp2p.peerId, blockstore)
+
+libp2p.handle(BITSWAP_PROTOCOL, miniswap.handler)
+```
---
+++ index.js
@@ -0,0 +1,91 @@
+import { pipe } from 'it-pipe'
+import * as lp from 'it-length-prefixed'
+import { transform } from 'streaming-iterables'
+import { Message } from './message.js'
+
+/**
+ * @typedef {import('multiformats').CID} CID
+ */
+
+export const BITSWAP_PROTOCOL = '/ipfs/bitswap/1.2.0'
+
+export class Miniswap {
+  /**
+   * @param {import('@libp2p/interfaces/peer-id').PeerId} peerId
+   * @param {{ get (cid: CID): Promise<Block|undefined>, has (cid: CID): Promise<boolean> }} blockstore
+   */
+  constructor (peerId, blockstore) {
+    this._peerId = peerId
+    this._blockstore = blockstore
+    this._handler = this._handler.bind(this)
+  }
+
+  get handler () {
+    return this._handler
+  }
+
+  /** @type {import('@libp2p/interfaces/registrar').StreamHandler} */
+  async _handler ({ connection, stream: inStream }) {
+    try {
+      await pipe(
+        inStream,
+        lp.decode(),
+        async source => {
+          for await (const data of source) {
+            const message = Message.decode(data)
+            const { stream: outStream } = await connection.newStream(BITSWAP_PROTOCOL)
+            await pipe(
+              this._processWantlist(message),
+              lp.encode(),
+              outStream
+            )
+          }
+        }
+      )
+    } catch (err) {
+      console.error(`${connection.remotePeer}: stream error`, err)
+    }
+  }
+
+  /** @param {Wantlist} wantlist */
+  _processWantlist (wantlist) {
+    return pipe(
+      wantlist.entries.filter(e => !e.cancel),
+      transform(5, async entry => {
+        if (entry.wantType === Entry.WantType.Block) {
+          const raw = await this._blockstore.get(entry.cid)
+          if (raw) {
+            return new Block(entry.cid, raw)
+          } else if (entry.sendDontHave) {
+            return new BlockPresence(entry.cid, BlockPresence.Type.DontHave)
+          }
+        } else if (entry.wantType === Entry.WantType.Have) {
+          const exists = await this._blockstore.has(entry.cid)
+          const type = exists ? BlockPresence.Type.Have : BlockPresence.Type.DontHave
+          return new BlockPresence(entry.cid, type)
+        }
+      }),
+      async function * (source) {
+        let message = new Message()
+        for await (const blockOrPresence of source) {
+          if (blockOrPresence instanceof Block) {
+            if (!message.addBlock(blockOrPresence)) {
+              yield message.encode()
+              message = new Message()
+              message.addBlock(blockOrPresence)
+            }
+          } else if (blockOrPresence instanceof BlockPresence) {
+            if (!message.addBlockPresence(blockOrPresence)) {
+              yield message.encode()
+              message = new Message()
+              message.addBlockPresence(blockOrPresence)
+            }
+          }
+        }
+        if (message.blocks.length || message.blocksPresences.length) {
+          yield message.encode()
+        }
+      }
+    )
+  }
+}
