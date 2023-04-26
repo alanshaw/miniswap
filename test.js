64e5b3922c1631dd96b36bfcefd67db3ac12d7de
Alan Shaw <alan.shaw@protocol.ai>
2023-04-26 10:35:21 +0100

    fix: prefix encoding (#3)
    Copy pasta bug.

--- test.js
+++ test.js
@@ -5,9 +5,10 @@ import { noise } from '@chainsafe/libp2p-noise'
 import { mplex } from '@libp2p/mplex'
 import { createBitswap } from 'ipfs-bitswap'
 import { MemoryBlockstore } from 'blockstore-core/memory'
-import { fromString, toString } from 'uint8arrays'
+import { fromString, toString } from 'multiformats/bytes'
 import * as raw from 'multiformats/codecs/raw'
 import { sha256 } from 'multiformats/hashes/sha2'
+import { blake2b256 } from '@multiformats/blake2/blake2b'
 import { CID } from 'multiformats/cid'
 import { multiaddr } from '@multiformats/multiaddr'
 import { Miniswap, BITSWAP_PROTOCOL } from './index.js'
@@ -52,7 +53,54 @@ test('should bitswap a single CID', async t => {
   bitswap.start()
 
   console.log(`bitswapping ${cid}`)
-  const retrievedData = await bitswap.get(cid)
+  const retrievedData = await bitswap.want(cid)
+
+  t.is(toString(retrievedData), toString(data))
+})
+
+test('should bitswap a CID that uses blake2b', async t => {
+  const clientBlockstore = new MemoryBlockstore()
+  const client = await createLibp2p({
+    transports: [webSockets()],
+    streamMuxers: [mplex()],
+    connectionEncryption: [noise()]
+  })
+
+  const bitswap = createBitswap(client, clientBlockstore, {
+    hashLoader: { getHasher: async () => blake2b256 }
+  })
+  console.log('starting client')
+  await client.start()
+
+  // create blockstore and add data
+  const serverBlockstore = new MemoryBlockstore()
+  const data = fromString(`TEST DATA ${Date.now()}`)
+  const hash = await blake2b256.digest(data)
+  const cid = CID.create(1, raw.code, hash)
+  await serverBlockstore.put(cid, data)
+
+  const serverAddr = '/ip4/127.0.0.1/tcp/1338/ws'
+  const server = await createLibp2p({
+    addresses: { listen: [serverAddr] },
+    transports: [webSockets()],
+    streamMuxers: [mplex()],
+    connectionEncryption: [noise()]
+  })
+
+  const miniswap = new Miniswap(serverBlockstore)
+  server.handle(BITSWAP_PROTOCOL, miniswap.handler)
+
+  console.log('starting server')
+  await server.start()
+
+  console.log(`dialing ${serverAddr}/p2p/${server.peerId}`)
+  await client.dial(multiaddr(`${serverAddr}/p2p/${server.peerId}`))
+
+  console.log('starting bitswap')
+  bitswap.start()
+
+  console.log(`bitswapping ${cid}`)
+  const retrievedData = await bitswap.want(cid)
 
   t.is(toString(retrievedData), toString(data))
 })
